28ccdbe72e9c4a6532df10a420fff5775bdcc66d
Jorge Ceniceros <143548160+jctorres10@users.noreply.github.com>
2024-03-01 12:02:13 -0700

    Create pantalla2.dart

---
+++ lib/pantalla2.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Pantalla2 extends StatelessWidget {
+  const Pantalla2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final argumento = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pantalla2'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(argumento),
+          ElevatedButton(
+            child: const Text("Regresar a Pantalla 1"),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      )),
+    );
+  }
+}
